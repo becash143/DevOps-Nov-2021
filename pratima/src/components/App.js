dd8cd73cb924e8eaf3320adabd592e8b5d817e95
pratimagurung <pratimagurung589@gmail.com>
2021-11-25 19:50:57 +0545

    Docker commit

---
+++ pratima/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import AddName from "./AddName";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AddName />
+    </div>
+
+  );
+}
+
+export default App;
